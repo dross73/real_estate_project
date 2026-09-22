53873f151301e9aa8c9c38d111c25771fbb65311
dross73 <evildead73@gmail.com>
2026-09-22 14:00:36 -0500

    KAN-74: update site settings service fixture

--- frontend/src/app/services/site-settings.service.spec.ts
+++ frontend/src/app/services/site-settings.service.spec.ts
@@ -31,6 +31,8 @@ describe('SiteSettingsService', () => {
     show_about: true,
     show_contact: true,
     show_testimonials: false,
+    enable_contact_requests: true,
+    enable_showing_requests: true,
     listing_photo_max_count: 50,
     hard_listing_photo_max_count: 50,
     updated_at: null,
