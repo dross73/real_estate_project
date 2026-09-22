f1a64e1d6bc6dac6b87b40cdc4fbe7619a1bb58c
dross73 <evildead73@gmail.com>
2026-09-22 13:52:07 -0500

    KAN-74: register public inquiry API

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ from app.api.listing_photos import router as listing_photos_router
 from app.api.notification_settings import router as notification_settings_router
 from app.api.offices import admin_router as offices_router, public_router as public_offices_router
 from app.api.public_engagement import router as public_engagement_router
+from app.api.public_inquiries import router as public_inquiries_router
 from app.api.public_listings import router as public_listings_router
 from app.api.saved_searches import router as saved_searches_router
 from app.api.site_settings import (
@@ -69,6 +70,7 @@ app.include_router(notification_settings_router)
 app.include_router(offices_router)
 app.include_router(public_offices_router)
 app.include_router(public_engagement_router)
+app.include_router(public_inquiries_router)
 app.include_router(public_listings_router)
 app.include_router(saved_searches_router)
 app.include_router(site_settings_router)
